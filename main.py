3323fc6de4bd2c038b29bfda718eb6b3eb8e6564
Reddersc022 <64492350+Reddersc022@users.noreply.github.com>
2020-07-31 18:28:06 +0100

    First Upload

---
+++ main.py
@@ -0,0 +1,23 @@
+"""
+Converts Ducky Script to Arduino Script
+Works for: REM, STRING, GUI, DELAY, and ENTER
+"""
+
+with open("infile.txt", "r") as f:
+	text = f.read()
+
+with open("outfile.txt", "w") as f:
+	f.write("""#include <DigiKeyboard.h>\nvoid setup() {
+	DigiKeyboard.sendKeystroke(0);
+	DigiKeyboard.delay(500);""")
+	for i in text.split("\n"):
+		i = i.replace("REM", "//")
+		if i[:6] == "STRING":
+			i = "DigiKeyboard.print(\"%s\")" % i[7:]
+		i = i.replace("GUI", "DigiKeyboard.sendKeystroke(KEY_R, MOD_GUI_LEFT)")
+		if i[:5] == "DELAY":
+			i = "DigiKeyboard.delay(%s)" % i[6:]
+		i = i.replace("ENTER", "DigiKeyboard.sendKeystroke(KEY_ENTER)")
+		f.write("\n	%s;" % i)
+	f.write("""\n}
+\nvoid loop() {\n\n}""")
